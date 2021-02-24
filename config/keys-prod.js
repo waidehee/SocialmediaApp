bb1a47f4e5b4ffaf473645bb65549e36a606ed32
waidehee <66438758+waidehee@users.noreply.github.com>
2021-02-24 18:39:46 +0530

    Delete keys-prod.js

--- config/keys-prod.js
+++
@@ -1,5 +0,0 @@
-module.exports ={
-    MongoURI :'mongodb+srv://socialmediaapp:socialmediaapp1@socialmediaapp.qx7fl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    GoogleClientID: '647918460976-2usb95t159sa86ojjpr4h6hvtre3bjn0.apps.googleusercontent.com',
-    GoogleClientSecret: '2oEhLfvktb0kAAf4lC2_1QQX'
-}
