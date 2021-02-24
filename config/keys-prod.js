6b68c46f6a981f7deeace6a974827cdef39303d5
waidehee <waideheezone@gmail.com>
2021-02-24 18:51:51 +0530

    config

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,5 +1,5 @@
 module.exports ={
-    MongoURI :'mongodb+srv://socialmediaapp:socialmediaapp1@socialmediaapp.qx7fl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    GoogleClientID: '647918460976-2usb95t159sa86ojjpr4h6hvtre3bjn0.apps.googleusercontent.com',
-    GoogleClientSecret: '2oEhLfvktb0kAAf4lC2_1QQX'
+    MongoURI :process.env.MONGO_URI,
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: process.env.GOOGLE_CLIENT-SECRET
 }
