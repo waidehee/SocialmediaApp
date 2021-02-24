7902d23098e31be0bb2678f3ebc3393d0b232296
waidehee <waideheezone@gmail.com>
2021-02-24 19:03:43 +0530

    config error check

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,5 +1,5 @@
 module.exports ={
     MongoURI :process.env.MONGO_URI,
     GoogleClientID: process.env.GOOGLE_CLIENT_ID,
-    GoogleClientSecret: process.env.GOOGLE_CLIENT-SECRET
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
