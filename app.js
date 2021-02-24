20fc97ac830766727ed524a1aca8c0f8308dcc3c
waidehee <waideheezone@gmail.com>
2021-02-24 15:47:42 +0530

    changes made

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 // Load Modules
 const express= require('express');
+const Handlebars= require('handlebars');
 const exphbs= require('express-handlebars');
+const {allowInsecurePrototypeAccess} =require(`@handlebars/allow-prototype-access`);
 const mongoose= require('mongoose');
+const passport= require('passport');
+const session = require("express-session");
+const bodyParser = require('body-parser');
+const cookieParser= require('cookie-parser');
 
 //Connect to MongoUri
 const keys= require('./config/keys');
+const Users = require('./models/users');
+
+require('./passport/google-passport');
 
 //initialise application
 const app= express();
+//Express config
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+      extended: false
+  }));
+app.use(bodyParser.json());
+app.use(session({ secret: 'keyboard cat' ,
+resave: true,
+saveUninitialized:true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//set global variable for user
+app.use((req,res,next) =>{
+    res.locals.user =req.user || null;
+    next();
+});
 
 //setup template
 app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
+    defaultLayout: 'main',
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
 }));
 app.set('view engine', 'handlebars');
 
@@ -19,6 +47,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 //connect to remote databse
+mongoose.Promise = global.Promise;
 mongoose.connect(keys.MongoURI,{
     useNewUrlParser: true
 })
@@ -39,6 +68,32 @@ app.get('/',(req,res)=> {
 
 app.get('/about',(req,res)=>{
     res.render('about');
+});
+
+//Google auth route
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/l' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/profile');
+  });
+
+app.get('/profile',(req,res) =>{
+    Users.findById({_id: req.user._id})
+    .then((user) =>{
+        res.render('profile', {
+            user: user});
+    });
+    
+});
+
+//Handle user logout route
+app.get('/logout', (req,res) =>{
+    req.logout();
+    res.redirect('/');
 });
 
 app.listen(port,()=>{
