3297649151b9a31632de29b6ea211b77e24107dc
waidehee <waideheezone@gmail.com>
2021-02-25 09:48:43 +0530

    facebook dev

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -2,7 +2,7 @@ module.exports ={
     MongoURI :process.env.MONGO_URI,
     GoogleClientID: process.env.GOOGLE_CLIENT_ID,
     GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    FcebookClientId: process.env.FACEBOoK_CLIENT_ID,
+    FacebookClientID: process.env.FACEBOOK_CLIENT_ID,
     FacebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET
 
 }
